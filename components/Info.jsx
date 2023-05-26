726b78a6092ccf1260a55f9a79b1c9932f94db99
hakimisch <hakimizaimi@gmail.com>
2023-05-26 22:39:10 +0800

    removed typewriter effect :(

--- components/Info.jsx
+++ components/Info.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Typed} from 'react-typed'
 
 const Info = () => {
   return (
@@ -7,11 +6,9 @@ const Info = () => {
             <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
                 <div className="lg:col-span-2 my-4">
                     <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold text-gray-100 py-6 pb-12">A place for keyboard lovers and newbies to discover what makes keyboard click and clack!</h1>
-                    <Typed 
-                     className="md:text-6xl sm:text-5xl text-2xl font-extrabold pl-2 text-gray-100"
-                     strings={['Clack?','Thock?','Creamy?','Clicky?']}
-                     typeSpeed={80}
-                     backSpeed={40} loop />
+                    <p className="md:text-6xl sm:text-5xl text-2xl font-extrabold pl-2 text-gray-100">Clack?,Thock?,Creamy?,Clicky?</p>
+                      
+                    
                     <p className='md:text-2xl sm:text-xl font-semibold py-4 pt-8 text-gray-100'>Check out below guides to have an idea about what you want in your next keyboard</p>
                 </div>
             </div>
